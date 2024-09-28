ccfc49b1c05f14d0dc7d552f8c7c29609f398443
MaxYbrNotebook <MaxYbrNotebook@DESKTOP-83HACU0>
2024-09-27 22:55:05 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoDelegado2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDelegado2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            saludoDelegado creacionDelegado = new saludoDelegado(Bienvenida.SaludoBienvenida);
+
+            creacionDelegado();
+
+            Console.ReadKey();
+
+            creacionDelegado = new saludoDelegado(Despedida.SaludoDespedida);
+
+            creacionDelegado();
+
+            Console.ReadKey();
+        }
+
+        delegate void saludoDelegado();
+    }
+
+    class Bienvenida
+    {
+
+        public static void SaludoBienvenida()
+        {
+            Console.WriteLine("Este es un saludo de bienvenida...");
+        }
+
+    }
+
+    class Despedida
+    {
+
+        public static void SaludoDespedida()
+        {
+            Console.WriteLine("Este es un saludo de despedida...");
+        }
+    }
+}
